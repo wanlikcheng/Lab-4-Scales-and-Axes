425043890163d6cb87fcb03f0522ee3f9769bb2f
Wanli <chengwj@bc.edu>
2021-09-30 22:59:49 -0400

    axis + tooltip

--- script.js
+++ script.js
@@ -15,10 +15,6 @@ d3.csv('wealth-health-2014.csv', d=>{
         const width = 650 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-        // // Create SVG element
-        // const width = 650;
-        // const height = 500;
-
         const svg = d3.select(".chart")
             .append("svg")
             .attr("width", width + margin.left + margin.right)
@@ -34,13 +30,17 @@ d3.csv('wealth-health-2014.csv', d=>{
             .domain(d3.extent(data, d => d.LifeExpectancy))
             .range([height, 0])
         
+        const popScale = d3.scaleLinear()
+            .domain(d3.extent(data, d => d.Population))
+            .range([4, 20])
+        
         svg.selectAll("circle")
             .data(healthData)
             .enter()
             .append("circle")
             .attr("cx", d=>xScale(d.Income))
             .attr("cy", d=>yScale(d.LifeExpectancy))
-            .attr("r", 4)
+            .attr("r", d => popScale(d.Population))
             .attr("fill", function(d) {
                 if(d.Region === "East Asia & Pacific") {
                     return "blue";
@@ -61,13 +61,32 @@ d3.csv('wealth-health-2014.csv', d=>{
                     return "gold";
                 }
             })
-            .attr("opacity", 65)
-            .attr("stroke", "black");
+            .attr("opacity", 0.6)
+            .attr("stroke", "black")
+            .on("mouseenter", (event, d) => {
+                const pos = d3.pointer(event, window); // pos = [x,y]
+                console.log(d.Country, pos);
+                d3.select(".tooltip")
+                .style("display", "block")
+                .style("top", (pos[1] + 5) + "px")
+                .style("left", (pos[0] + 5) + "px").html(`
+                    <div>Country: ${d.Country} </div>
+                    <div>Region: ${d.Region} </div>
+                    <div>Population: ${d3.format(",")(d.Population)} </div>
+                    <div>Income: ${d3.format(",")(d.Income)} </div>
+                    <div>Life Expectancy: ${d.LifeExpectancy} </div>
+                    `
+                    );
+            })
+            .on("mouseleave", (event, d) => {
+                d3.select(".tooltip").style("display", "none");
+            });
 
         
         // using axis
         const xAxis = d3.axisBottom()
 	        .scale(xScale)
+            .ticks(5, "s")
         const yAxis = d3.axisLeft()
 	        .scale(yScale)
 
@@ -78,8 +97,40 @@ d3.csv('wealth-health-2014.csv', d=>{
             .call(xAxis);
         svg.append("g")
             .attr("class", "axis y-axis")
-            .attr("transform", `translate(0, ${width})`)
+            .attr("transform", `translate(0, ${0})`)
             .call(yAxis);
-    
+        
+        // adding labels
+        svg.append("text")
+            .attr("class", "xlabel")
+            .attr('x', width - 65)
+            .attr('y', height - 10)
+            .attr("alignment-baseline", "baseline")
+            .text("Income")
+
+        svg.append("text")
+            .attr("class", "ylabel")
+            .attr('x', 10)
+            .attr('y', 0)
+            .attr("alignment-baseline", "baseline")
+            .text("Life Expectancy")
+        
+        // encoding region
+        
+        const color = d3.scaleOrdinal(d3.schemeTableau10);
+
+        // tooltip
+        const legend = svg.append("g")
+
+        legend.selectAll("rect")
+            .data(healthData)
+            .enter()
+            .append("rect")
+            .attr("x", 600)
+            .attr("y", 500)
+            .width(10)
+            .height(10)
+            
+
 
     })
